ab0c34ce9e4ac18cff9be4407a8eb7f65015abce
qinyan <1367987599@qq.com>
2017-02-21 14:51:07 +0800

    “修改第1次提交，根据用户ID查找用户”

--- src/main/java/com/raycloud/newtest/dao/imp/UserImp.java
+++ src/main/java/com/raycloud/newtest/dao/imp/UserImp.java
@@ -6,6 +6,7 @@ import com.raycloud.newtest.entity.User;
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by 001117020015 on 2017/2/17.
@@ -13,6 +14,7 @@ import javax.annotation.Resource;
 public class UserImp extends SqlMapClientDaoSupport implements IUserDAO {
     @Override
     public User queryById(int id) {
+        /*根据ID查找用户*/
         User user=(User)getSqlMapClientTemplate().queryForObject("queryById",id);
         return  user;
     }
@@ -20,5 +22,9 @@ public class UserImp extends SqlMapClientDaoSupport implements IUserDAO {
     public User queryByName(String name) {
         User user=(User)getSqlMapClientTemplate().queryForObject("queryByName",name);
         return  user;
+    }
+    @Override
+    public java.util.List<User> queryAll() {
+        return null;
     }
 }
