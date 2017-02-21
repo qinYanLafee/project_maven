6fd90938cbb22e90b1fa5e1e8f623653c7dc6c25
qinyan <1367987599@qq.com>
2017-02-21 15:00:51 +0800

    “修改第5次提交，增加属性年龄”

--- src/main/java/com/raycloud/newtest/entity/User.java
+++ src/main/java/com/raycloud/newtest/entity/User.java
@@ -11,12 +11,13 @@ public class User {
     private   int      id;
     private   String    name;
     private   String    password;
+    private   int       age;
 
     public User() {
         super();
     }
 
-    public User(int id, String name,String password) {
+    public User(int id, String name,String passworde) {
         this.id=id;
         this.name = name;
         this.password = password;
